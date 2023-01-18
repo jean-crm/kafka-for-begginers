164fd040a2c99e998aef8d34fc2743b0c366e498
Jean <jeanaxe@gmail.com>
2023-01-18 16:33:34 -0500

    Implement the Basic Producer Demo implementation

---
+++ src/main/java/org/example/ProducerDemo.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // 1. Properties section
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 2. Create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // 3. Create a produce record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>("first_topic", "hello world");
+
+        // 4. Send Data
+        producer.send(record);
+
+        // 5. flush and close
+        producer.flush();
+        producer.close();
+
+    }
+}
