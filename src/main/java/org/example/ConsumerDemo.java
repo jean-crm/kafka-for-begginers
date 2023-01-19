f34edceb5b26e089f8d32830ace8e584d9c35763
Jean <jeanaxe@gmail.com>
2023-01-19 11:28:32 -0500

    Consumer Demo example completed

--- src/main/java/org/example/ConsumerDemo.java
+++ src/main/java/org/example/ConsumerDemo.java
@@ -1,15 +1,21 @@
 package org.example;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class ConsumerDemo {
+
+    public static final int CONSUMER_POLL_DURATION = 100;
+
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
 
@@ -29,11 +35,18 @@ public class ConsumerDemo {
         // 2. Create the consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 
-        // 3. Subscribe the consumer to a topic
-        consumer.subscribe(Collections.singleton(topic));
+        // 3. Subscribe the consumer to a topic(s)
+        consumer.subscribe(Arrays.asList(topic));
 
-
-
+        // 4. Poll for new data
+        while(true){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(CONSUMER_POLL_DURATION)); // new in Kafka 2.0.0
+            for (ConsumerRecord<String, String> record: records) {
+                logger.info("Key: "+record.key()+", Value: "+record.value());
+                logger.info("Partition: "+record.partition()+", Offset: "+ record.offset());
+            }
+        }
 
     }
 }
