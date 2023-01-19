0339d4be8ab78f7cc2db8f583000a41910426e34
Jean <jeanaxe@gmail.com>
2023-01-19 12:08:24 -0500

    Example using new Consumer Group to see the rebalancing process

---
+++ src/main/java/org/example/ConsumerDemoGroups.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoGroups {
+
+    public static final int CONSUMER_POLL_DURATION = 100;
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class.getName());
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-fifth-application";
+        String autoOffsetResetConfig = "earliest";
+        String topic = "first_topic";
+
+        // 1. Create consumer configuration
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetResetConfig);
+
+        // 2. Create the consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // 3. Subscribe the consumer to a topic(s)
+        consumer.subscribe(Arrays.asList(topic));
+
+        // 4. Poll for new data
+        while(true){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(CONSUMER_POLL_DURATION)); // new in Kafka 2.0.0
+            for (ConsumerRecord<String, String> record: records) {
+                logger.info("Key: "+record.key()+", Value: "+record.value());
+                logger.info("Partition: "+record.partition()+", Offset: "+ record.offset());
+            }
+        }
+
+    }
+}
